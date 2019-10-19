79476adb6c659ee50e100f12e2337a895cc6e61e
Pier Fiedorowicz <pierfied@email.arizona.edu>
2019-10-19 15:16:10 -0700

    Basic file structure.

---
+++ src/healcov.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pierfied on 10/19/19.
+//
+
+#include "healcov.h"
---
+++ src/healcov.h
@@ -0,0 +1,8 @@
+//
+// Created by pierfied on 10/19/19.
+//
+
+#ifndef HEALCOV_HEALCOV_H
+#define HEALCOV_HEALCOV_H
+
+#endif //HEALCOV_HEALCOV_H
